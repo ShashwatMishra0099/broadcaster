6953c55aed9c5e51275268da5bd13de7cb665c06
Shashwat <163753719+ShashwatMishra0099@users.noreply.github.com>
2024-09-29 12:11:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_TOKEN")  # Bot Token from @BotFather
-ADMIN_ID = os.getenv("ADMIN_ID")     # Your Telegram user ID (optional, to restrict bot control)
+ADMIN_ID = os.getenv("ADMIN_ID")     # Your Telegram user ID for restricting access
